b4830226d126fe79b927ef82ba7574727e07859d
DESKTOP-MBRQ737\Eva <evastylova@gmail.com>
2023-02-04 18:01:38 +0100

    Zpravovani ukolu 3

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 // tady je místo pro náš program
 
+let osoba = document.querySelector('.uzivatel');
+
+let jmeno = prompt('Zadej jméno');
+let prijmeni = prompt('Zadej příjmení');
+let vek = Number(prompt('Zadej věk'));
+let barva = prompt('Zadej svoji oblíbenou barvu - anglicky');
+
+const vystup = (jmeno + ' ' + prijmeni + ' ' + vek);
+
+osoba.style.color = barva;
+osoba.style.fontStyle = "italic";
+
+osoba.innerHTML = vystup;
 
 
 
